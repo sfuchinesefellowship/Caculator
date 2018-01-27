3cc1306378d2159c26a6bb7498516f1e1678336b
Rui Wang <rwa56@lib7000-rcmac04.lib.sfu.ca>
2018-01-27 11:06:46 -0800

    Initial Commit

---
+++ Caculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Caculator
+//
+//  Created by Rui Wang on 18/1/27.
+//  Copyright © 2018年 Rui Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
